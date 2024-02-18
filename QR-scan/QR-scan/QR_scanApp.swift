4fd4491ed9501cd026a7f163b18ef9b32c3bb896
kimhyeri <hyer1k@naver.com>
2024-02-18 18:34:08 +0900

    initial commit

---
+++ QR-scan/QR-scan/QR_scanApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_scanApp.swift
+//  QR-scan
+//
+//  Created by Hye Ri Kim on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct QR_scanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
